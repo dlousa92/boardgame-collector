3bfec2cf224218ddba5cb595ee98ec58ee669087
Daniel Lousa <dlousa92@gmail.com>
2018-06-11 15:14:54 -0400

    make sure server is working properly

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
 
+app.get('/', (request, response) => {
+  response.send('All systems go')
+})
+
 app.listen(4000, () => {
   console.log('app listening on port 4000 copy that')
 })
