98bdd10a0eb7e6175e1232c4c6459cd339ec3a62
Daniel Lousa <dlousa92@gmail.com>
2018-06-12 11:28:35 -0400

    add error message if database does not connect

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose')
 
 mongoose.connect('mongod://localhost/boardgamecollector')
-
 mongoose.Promise = Promise
+
+// error if database connection does not work
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 module.exports = mongoose
