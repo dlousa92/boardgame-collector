26d265b8d5b72f22c1feaff2009968041d78dd61
Daniel Lousa <dlousa92@gmail.com>
2018-06-13 15:34:56 -0400

    create user controller and fix typo

---
+++ controllers/user.js
@@ -0,0 +1,22 @@
+const User = require('../models/User')
+const { Boardgame } = require('../models/Boardgame')
+
+module.exports = {
+  collection: (req, res) => {
+
+  },
+  login: (req, res) => {
+
+  },
+  createLogin: (req, res) => {
+
+  },
+  signUp: (req, res) => {
+
+  },
+  createSignUp: (req, res) => {
+
+  },
+  logout: (req, res) => {
+  }
+}
