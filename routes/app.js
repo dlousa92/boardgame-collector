7163f2036acc2cdb545cd88e97da6c86357b195d
Daniel Lousa <dlousa92@gmail.com>
2018-06-13 08:59:41 -0400

    correctly link to right app js

--- routes/app.js
+++ routes/app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const applicationController = require('../controllers/application.js')
+const applicationController = require('../controllers/app.js')
 
 router.get('/', applicationController.index)
 
