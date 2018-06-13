be874d0ca37a6215b5912fb17efd616bb65ad077
Daniel Lousa <dlousa92@gmail.com>
2018-06-13 16:56:40 -0400

    create boardgame controller

---
+++ controllers/boardgame.js
@@ -0,0 +1,14 @@
+const Boardgame = require('../models/Boardgame')
+const BoardgameInstance = require('../models/Boardgame-instance')
+
+module.exports = {
+  new: (req, res) => {
+
+  },
+  create: (req, res) => {
+
+  },
+  updateRating: (req, res) => {
+
+  }
+}
