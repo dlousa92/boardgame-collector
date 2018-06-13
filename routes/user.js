bd6bebccab0e4c683b4ead9134e28e404c10c523
Daniel Lousa <dlousa92@gmail.com>
2018-06-13 16:20:12 -0400

    export user routes

--- routes/user.js
+++ routes/user.js
@@ -6,5 +6,7 @@ router.get('/login', user.login)
 router.post('/login', user.createLogin)
 router.get('/new', user.signUp)
 router.post('/new', user.createSignUp)
-router.get('/:id', user.collection)
+router.get('/:id', user.show)
 router.get('/logout', user.logout)
+
+module.exports = router
