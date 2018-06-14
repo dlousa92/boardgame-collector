cc6aea622d24f944b2181e0a8062e1364fb56ba7
Daniel Lousa <dlousa92@gmail.com>
2018-06-14 10:58:27 -0400

    create boardgame instance controllers

---
+++ controllers/boardgame-instance.js
@@ -0,0 +1,13 @@
+const BoardgameInstance = require('../models/Boardgame-instance')
+const Boardgame = require('../models/Boardgame')
+
+module.exports = {
+  show: (req, res) => {
+
+  },
+  new: (req, res) => {
+
+  },
+  create: (req, res) => {
+  }
+}
