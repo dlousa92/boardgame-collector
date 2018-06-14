a570f1627689cec40ebcf9075e96bacbd690907f
Daniel Lousa <dlousa92@gmail.com>
2018-06-14 12:12:23 -0400

    fix single qoutes issue

--- routes/app.js
+++ routes/app.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const router = express.Router()
-const userController = require('../controllers/user.js')
+const user = require('../controllers/user.js')
 
 router.get('/', function (req, res) {
   res.redirect('/login')
 })
 
 router.get('/login', function (req, res) {
-  res.render('user/login', {title: "Express"})
+  res.render('user/login', {title: 'Express'})
 })
 
 module.exports = router
