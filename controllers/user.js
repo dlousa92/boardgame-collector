cad5f84151cc91f036e2c37c5ac730f19577e0b7
Daniel Lousa <dlousa92@gmail.com>
2018-06-14 10:55:37 -0400

    add testing to make sure everything is working

--- controllers/user.js
+++ controllers/user.js
@@ -1,9 +1,9 @@
 const User = require('../models/User')
-const { Boardgame } = require('../models/Boardgame')
+const Boardgame = require('../models/Boardgame')
 
 module.exports = {
   show: (req, res) => {
-
+    res.send('collection goes here')
   },
   login: (req, res) => {
     res.send('hello')
@@ -12,11 +12,12 @@ module.exports = {
 
   },
   signUp: (req, res) => {
-
+    res.send('welcome fren')
   },
   createSignUp: (req, res) => {
 
   },
   logout: (req, res) => {
+    res.send('goodbye')
   }
 }
