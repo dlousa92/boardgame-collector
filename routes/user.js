d4acc46651e1352a94612b7adcbedc64cc2d7b6b
Daniel Lousa <dlousa92@gmail.com>
2018-06-14 14:31:26 -0400

    change new route to signUp route

--- routes/user.js
+++ routes/user.js
@@ -4,8 +4,8 @@ const user = require('../controllers/user.js')
 
 router.get('/login', user.login) // will log user in
 router.post('/login', user.createLogin)
-router.get('/new', user.signUp) // will sign user up
-router.post('/new', user.createSignUp)
+router.get('/signUp', user.signUp) // will sign user up
+router.post('/signUp', user.createSignUp)
 router.get('/:id', user.show) // will show users library of boardgames
 router.get('/logout', user.logout) // will log user out
 
