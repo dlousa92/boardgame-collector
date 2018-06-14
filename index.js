b3cba59deb595e774d5d6d57cd74cd76fc22c3c9
Daniel Lousa <dlousa92@gmail.com>
2018-06-14 16:03:02 -0400

    comment out boardgame-instance router for now

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.set('view engine', 'hbs')
 app.use('/', require('./routes/app'))
 app.use('/user', require('./routes/user'))
 app.use('/boardgame', require('./routes/boardgame'))
-app.use('boardgame-instance', require('./routes/boardgame-instance'))
+// app.use('boardgame-instance', require('./routes/boardgame-instance'))
 
 app.set('port', process.env.PORT || 4000)
 app.listen(4000, () => {
