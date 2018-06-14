02ab301413af2de5cc48b858e4ce21c44d883689
Daniel Lousa <dlousa92@gmail.com>
2018-06-14 14:00:15 -0400

    no need to have user declared in the route path so deleted

--- routes/user.js
+++ routes/user.js
@@ -2,11 +2,11 @@ const express = require('express')
 const router = express.Router()
 const user = require('../controllers/user.js')
 
-router.get('/login', user.login)
+router.get('/login', user.login) // will log user in
 router.post('/login', user.createLogin)
-router.get('/new', user.signUp)
+router.get('/new', user.signUp) // will sign user up
 router.post('/new', user.createSignUp)
-router.get('/:id', user.show)
-router.get('/logout', user.logout)
+router.get('/:id', user.show) // will show users library of boardgames
+router.get('/logout', user.logout) // will log user out
 
 module.exports = router
