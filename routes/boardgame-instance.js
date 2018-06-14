d3a04dfbc2ae1fa39f281151f431acc55259cab1
Daniel Lousa <dlousa92@gmail.com>
2018-06-14 10:55:12 -0400

    create boardgame instance routes

---
+++ routes/boardgame-instance.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const router = express.Router
+const boardgameInstance = require('../models/Boardgame-instance')
+
+router.get('boardgame-instance/:id', boardgameInstance.show)
+router.get('boardgame-instance/new', boardgameInstance.new)
+router.post('boardgame-instance/new', boardgameInstance.create)
