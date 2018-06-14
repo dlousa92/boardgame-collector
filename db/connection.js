cca8dc2ba6780e1bd691af664b4c17c2665c6601
Daniel Lousa <dlousa92@gmail.com>
2018-06-14 15:59:33 -0400

    set up connection to heroku

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost/boardgamecollector')
+if (process.env.NODE_ENV === 'production') {
+  mongoose.connect(process.env.MLAB_URL)
+} else {
+  mongoose.connect('mongodb://localhost/boardgamecollector')
+}
+
 mongoose.Promise = Promise
 
 // error if database connection does not work
--- index.js
+++ index.js
@@ -8,6 +8,7 @@ app.use('/user', require('./routes/user'))
 app.use('/boardgame', require('./routes/boardgame'))
 app.use('boardgame-instance', require('./routes/boardgame-instance'))
 
+app.set('port', process.env.PORT || 4000)
 app.listen(4000, () => {
   console.log('app listening on port 4000 copy that')
 })
