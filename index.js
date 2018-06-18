ead6b57a43a51fe5cba1cadee8a6f8b8d86ecafe
Daniel Lousa <dlousa92@gmail.com>
2018-06-18 10:28:01 -0400

    install body parser and hbs

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express')
+const hbs = require('hbs')
+const bodyParser = require('body-parser')
 const app = express()
-
-app.set('view engine', 'hbs')
 
 app.use('/', require('./routes/app'))
 app.use('/user', require('./routes/user'))
 app.use('/boardgame', require('./routes/boardgame'))
 app.use('/boardgame-instance', require('./routes/boardgame-instance'))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.set('view engine', 'hbs')
 
 app.set('port', process.env.PORT || 5000)
 app.listen(process.env.PORT || 5000)
