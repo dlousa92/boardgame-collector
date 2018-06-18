86ca21cfccaec45183f0e51488d962a54c45854d
Daniel Lousa <dlousa92@gmail.com>
2018-06-18 12:04:29 -0400

    seed boardgame database and create one user for testing

--- db/seed.js
+++ db/seed.js
@@ -1,7 +1,25 @@
-const data = require('./seedData.js')
+const boardgameData = require('./seedBoardgameData.js')
 const Boardgame = require('../models/Boardgame')
+const User = require('../models/User')
+const bcrypt = require('bcrypt-nodejs')
 
-data.forEach(game => {
+const createPassword = password =>
+  bcrypt.hashSync(password, bcrypt.genSaltSync(8), null)
+
+User.find({}).remove(() => {
+  let john = User.create({
+    local: {
+      name: 'John',
+      email: 'john@sample.com',
+      password: createPassword('john')
+    }
+  })
+    .then((user => {
+      user.save(err => console.log(err))
+    }))
+})
+
+boardgameData.forEach(game => {
   Boardgame.create({
     info: {
       title: game.name,
@@ -10,4 +28,9 @@ data.forEach(game => {
       rating: game.averageRating
     }
   })
+    .then(games => {
+      games.save(err => console.log(err))
+    })
 })
+
+// process.exit()
