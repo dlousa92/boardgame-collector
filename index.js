eb557838ad47e3ffebcce9cc2818a429de61960f
Daniel Lousa <dlousa92@gmail.com>
2018-06-19 13:13:42 -0400

    require all passport and session dependencies

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
 const express = require('express')
 const hbs = require('hbs')
+const flash = require('connect-flash')
+const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
+const session = require('express-session')
+const passport = require('passport')
+const methodOverride = require('method-override')
 const app = express()
+require('./config/passport')(passport)
 
+hbs.registerPartials(__dirname + "/views/partials")
 app.use(express.static(__dirname + '/public'))
+app.use(cookieParser())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.set('view engine', 'hbs')
+app.use(methodOverride('method'))
+app.use(
+  session({
+    secret: 'EXPRESS-IS-AWESOME',
+    saveUninitialized: true,
+    resave: false
+  })
+)
+app.use(flash())
+app.use(passport.initialize())
+app.use(passport.session())
+
 app.use('/', require('./routes/app'))
 app.use('/user', require('./routes/user'))
 app.use('/boardgame', require('./routes/boardgame'))
 app.use('/boardgame-instance', require('./routes/boardgame-instance'))
-app.use(bodyParser.urlencoded({ extended: true }))
-app.set('view engine', 'hbs')
 
 app.set('port', process.env.PORT || 5000)
 app.listen(process.env.PORT || 5000)
