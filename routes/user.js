d486804157f803d1c55af6e856b627e52faeb46d
Daniel Lousa <dlousa92@gmail.com>
2018-06-20 17:44:59 -0400

    move position of profile route above /:id route

--- routes/user.js
+++ routes/user.js
@@ -6,8 +6,8 @@ router.get('/login', user.login) // will log user in
 router.post('/login', user.createLogin)
 router.get('/signUp', user.signUp) // will sign user up
 router.post('/signUp', user.createSignUp)
+router.get('/profile', user.profile)
 router.get('/:id', user.show) // will show users library of boardgames
 router.get('/logout', user.logout) // will log user out
 router.put('/update', user.update)
-
 module.exports = router
