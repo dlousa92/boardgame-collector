160f1462345a9e979caf544c2b87f462ffddaec0
Daniel Lousa <dlousa92@gmail.com>
2018-06-21 12:47:39 -0400

    pass user object to library page so its possible to render collection

--- controllers/user.js
+++ controllers/user.js
@@ -6,10 +6,11 @@ module.exports = {
   show: (req, res) => {
     User.findOne({ _id: req.params.id })
       .populate({
-        path: 'library'
+        path: 'library',
+        options: { sort: { title: 1 } }
       })
       .then(user => {
-        res.render('user/library', {})
+        res.render('user/library', { user })
       })
       // user/5b27d657fe8f803744da9d7a for testing purposes
   },
@@ -38,12 +39,9 @@ module.exports = {
     res.redirect('/')
   },
   update: (req, res) => {
-    console.log(req.body)
-    
+    // console.log(req.body)
     req.user.library.push(req.body.gameId)
-    // you're saving the game after updating the user!
     req.user.save(err => {
-      // the user is req.user, not just user!
       res.redirect(`/user/${req.user.id}`)
     })
   },
